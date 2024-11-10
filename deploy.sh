cdc06e6fee44e29410c2cdafd152775e23671a76
Cyrus M. <61042997+cyrusmcc@users.noreply.github.com>
2024-11-10 16:31:53 -0600

    Remove demo blogs, update url

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/cyrusmcc/cyrusmcc.github.io.git master:gh-pages
+git push -f https://github.com/cyrusmcc/cyrusmcc.github.io.git main:gh-pages
 
 cd -
